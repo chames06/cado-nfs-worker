95eeb413d528d98eafb8054e55fa1a01d205100b
chamesddine <147053582+chames06@users.noreply.github.com>
2025-12-11 20:00:30 +0100

    Delete test_small.sh

--- test_small.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Test avec un nombre de 40 chiffres (faisable rapidement)
-N="1234567890123456789012345678901234567891"
-echo "Test avec N = $N (${#N} chiffres)"
-echo "test" > "/tmp/sieving/${N}_0_test.dat"
-echo "✓ Fichier créé"
-echo ""
-echo "Suivez avec: docker logs -f factorn-master"
